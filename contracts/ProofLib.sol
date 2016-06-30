1a562b08cb1fb42e6446b6dcab5acbe59e7496b4
Tjaden Hess <tjade273@gmail.com>
2016-06-30 13:07:00 -0400

    Flesh out proving library

---
+++ contracts/ProofLib.sol
@@ -0,0 +1,79 @@
+library ProofLib {
+
+    struct Proof {
+        address defender;
+        address challenger;
+
+        bytes32 lVal;
+        bytes32 rVal;
+        uint lIndex;
+        uint rIndex;
+
+        uint roundTime;
+        uint lastRound;
+
+        bytes32 currentVal;
+
+        //TODO: Implement non-participation resilliancy
+    }
+
+
+    function newChallenge(Proof storage self, address _defender, address _challenger, bytes32 seed, bytes32 result, uint difficulty, uint time) {
+        self.defender = _defender;
+        self.challenger = _challenger;
+
+        self.lVal = seed;
+        self.rVal = result;
+
+        self.lIndex = 0;
+        self.rIndex = difficulty;
+
+        self.roundTime = time;
+        self.lastRound = block.number;
+
+
+    }
+
+    function challenge(Proof storage self, bool correct) {
+        if (self.currentVal == 0 || msg.sender != self.challenger) throw;
+        if (correct) {
+            self.lIndex = (self.lIndex + self.rIndex) / 2;
+            self.lVal = self.currentVal;
+        } else {
+            self.rIndex = (self.lIndex + self.rIndex) / 2;
+            self.rVal = self.currentVal;
+        }
+
+        self.currentVal = 0;
+    }
+
+    function respond(Proof storage self, bytes32 hash) {
+        if (self.currentVal != 0 || msg.sender != self.defender) throw;
+        self.currentVal = hash;
+    }
+
+    function finalize(Proof storage self) returns(bool confirmed) { //returns true if challenge successfully disproves proposal
+
+        if (self.rIndex - self.lIndex <= 3) {
+            bytes32 hash = self.lVal;
+
+            for (uint i; i < self.rIndex - self.lIndex; i++) {
+                hash = sha3(hash);
+            }
+
+            if (hash == self.rVal) {
+                return false;
+            } else {
+                return true;
+            }
+        } else throw;
+    }
+
+
+     function getProof(Proof storage self) constant returns(uint,uint,bytes32,bytes32,bytes32){
+         return (self.lIndex, self.rIndex, self.lVal, self.currentVal, self.rVal);
+     }
+
+
+
+}
